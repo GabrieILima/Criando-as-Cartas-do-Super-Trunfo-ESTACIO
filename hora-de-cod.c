1ed03fd37e7a6c311011e3af90e4e10a269fa896
GabrieILima <147564907+GabrieILima@users.noreply.github.com>
2025-02-28 14:42:54 +0000

    Inclusã das funçoes de calculo de densidade e percapita

--- hora-de-cod.c
+++ hora-de-cod.c
@@ -11,6 +11,15 @@ typedef struct
     float area, PIB;
 } Carta;
 
+int densPolu(float a,int p){
+   float densidade = p/a;
+   return densidade;
+}
+
+int pibPerCapita(float produto,int pessoa){
+    float perCapita = pessoa/produto;
+    return perCapita;
+ }
 
 int main(){
     Carta carta1; // Define uma variável do tipo de estrutura Carta, criada acima
@@ -44,6 +53,8 @@ int main(){
     printf("Informe o PIB da cidade: \n");
     scanf("%f", &carta1.PIB);
 
+    
+
     // Exibindo os dados cadastrados
     printf("\n🔹 Cidade Cadastrada 🔹\n");
     printf("Estado: %c\n", carta1.estado);
@@ -53,6 +64,38 @@ int main(){
     printf("População: %d habitantes\n", carta1.populacao);
     printf("Pontos Turísticos: %d\n", carta1.pontosTuristicos);
     printf("PIB: %.2f bilhões\n", carta1.PIB);
+    
+
+    Carta carta2; // Define uma variável do tipo de estrutura Carta, criada acima
+
+
+    //Abaixo segue as solicitações para entrada dos dados das cartas
+    printf("Digite a qual estado a cidade pertence: \n");
+    scanf("%c", &carta2.estado);
+
+    getchar();//remove o \n deixado pelo scanf pois pode causar erros de conflito com o fgets
+
+    printf("Digite o codigo da Cidade: \n");
+    fgets(carta2.codigoCarta, 20, stdin);
+
+    carta2.codigoCarta[strcspn(carta2.codigoCarta, "\n")] = '\0'; // Remove o '\n' de fgets
+
+    printf("Digite o nome da cidade: \n");
+    fgets(carta2.cidade,50, stdin);
+
+    carta2.cidade[strcspn(carta2.cidade, "\n")] = '\0'; // Remove o '\n' de fgets
+
+    printf("Informe a área da cidade: \n");
+    scanf("%f", &carta2.area);
+
+    printf("Informe a população da cidade: \n");
+    scanf("%d", &carta2.populacao);
+
+    printf("Informe quantos pontos Turísticos a cidade possui: \n");
+    scanf("%d", &carta2.pontosTuristicos);
+
+    printf("Informe o PIB da cidade: \n");
+    scanf("%f", &carta2.PIB);
 
     return 0;
     
